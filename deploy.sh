15400ba567f98befd735b5a04c276812cc49c579
Graha <grahaindia@gmail.com>
2018-09-10 04:02:46 +0530

    Show result

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ then
     docker stack deploy -c docker-delta.yml.e ${STACK} 
     docker service update --env-add ACTIV_APP_ENDPOINT=app-${DEPLOYED} --env-add ALTER_APP_ENDPOINT=app-${NORMALIZED_VERSION} --env-add ALTER_APP_PARAM=backup ${STACK}_app-lb
     echo ${VERSION} > dark.version
+    cat docker-delta.yml.e
 elif [ $METHOD == 'A/B Testing' ] && [ -e deployed.version ]
 then
     echo "Deploying ABTesting"
@@ -22,6 +23,7 @@ then
     docker stack deploy -c docker-delta.yml.e ${STACK} 
     docker service update --env-add ACTIV_APP_ENDPOINT=app-${DEPLOYED} --env-add ALTER_APP_ENDPOINT=app-${NORMALIZED_VERSION} ${STACK}_app-lb
     echo ${VERSION} > dark.version
+    cat docker-delta.yml.e
 elif [ $METHOD == 'Canary' ] && [ -e deployed.version ]
 then
     echo "Deploying Canary"
@@ -30,9 +32,13 @@ then
     docker stack deploy -c docker-delta.yml.e ${STACK} 
     docker service update --env-add ACTIV_APP_ENDPOINT=app-${DEPLOYED} --env-add ALTER_APP_ENDPOINT=app-${NORMALIZED_VERSION} --env-add ACTIV_APP_PARAM=weight=4 --env-add ALTER_APP_PARAM=weight=1 ${STACK}_app-lb
     echo ${VERSION} > dark.version
+    cat docker-delta.yml.e
 else
     echo "Deploying Clean"
     sed "s/\\\${nversion}/${NORMALIZED_VERSION}/g;s/\\\${version}/${VERSION}/g" docker-clean.yml.t > docker-clean.yml.e
     echo ${VERSION} > deployed.version
     docker stack deploy -c docker-clean.yml.e ${STACK}
+    cat docker-clean.yml.e
 fi
+
+docker service ls 
