ecb12cc25602d717d1898e42a7d86fe5cc833e42
Giragadurai Vallirajan <grahaindia@gmail.com>
2018-09-16 02:39:03 +0530

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -8,8 +8,14 @@ CLEAN=$1
 STACK=$2
 
 
-if [ $CLEAN == 'Release' ]
-then
+if [ $CLEAN == 'Rollback' ] || [ $CLEAN == 'B' ] 
+    echo "Removing ${STACK}_app-${DARK}"
+    TD=`docker service inspect ${STACK}_app-${DARK}| grep com.docker.stack.image | cut -f 4 -d "\""`
+    docker service rm ${STACK}_app-${DARK} 
+    sleep 5
+    docker rmi -f ${TD}
+    docker service update --env-add ACTIV_APP_ENDPOINT=app-${DEPLOYED} --env-add ALTER_APP_ENDPOINT=app-${DEPLOYED} ${STACK}_app-lb
+else
     echo "Removing ${STACK}_app-${DEPLOYED}"
     TD=`docker service inspect ${STACK}_app-${DEPLOYED}| grep com.docker.stack.image | cut -f 4 -d "\""`
     docker service rm ${STACK}_app-${DEPLOYED} 
@@ -17,13 +23,6 @@ then
     docker rmi -f ${TD}
     docker service update --env-add ACTIV_APP_ENDPOINT=app-${DARK} --env-add ALTER_APP_ENDPOINT=app-${DARK} ${STACK}_app-lb
     cp ~/.deploy/dark.version ~/.deploy/deployed.version
-else
-    echo "Removing ${STACK}_app-${DARK}"
-    TD=`docker service inspect ${STACK}_app-${DARK}| grep com.docker.stack.image | cut -f 4 -d "\""`
-    docker service rm ${STACK}_app-${DARK} 
-    sleep 5
-    docker rmi -f ${TD}
-    docker service update --env-add ACTIV_APP_ENDPOINT=app-${DEPLOYED} --env-add ALTER_APP_ENDPOINT=app-${DEPLOYED} ${STACK}_app-lb
 fi
 
 rm ~/.deploy/dark.version
